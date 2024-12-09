eaf0d43b4a9a68eb802d05fdcbcfc8a261e8ca78
mavlekiev <mavlekiev@gmail.com>
2024-12-10 03:02:01 +0400

    feat: solved 10 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -297,8 +297,11 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) return i;
+  }
+  return -1;
 }
 
 /**
@@ -316,8 +319,15 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  let i = 10;
+  while (number !== 0) {
+    if (digit === number % 10) return true;
+    number = Math.trunc(num / i);
+    i *= 10;
+  }
+  return false;
 }
 
 /**
