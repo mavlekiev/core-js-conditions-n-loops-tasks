81cd8c75e238ae9b8f64c0f6a3b4c59604ce87e3
mavlekiev <mavlekiev@gmail.com>
2024-12-24 22:24:39 +0700

    feat: add solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -466,8 +466,19 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  let minElement = arr[0];
+  const arr1 = arr;
+  for (let i = 0; i < arr1.length; i += 1) {
+    for (let j = i; j < arr1.length; j += 1) {
+      if (arr1[j] < arr1[i]) {
+        minElement = arr1[j];
+        arr1[j] = arr1[i];
+        arr1[i] = minElement;
+      }
+    }
+  }
+  return arr;
 }
 
 /**
