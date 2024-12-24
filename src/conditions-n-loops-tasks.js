b4f7c0df4ae65aa4d1734109fbe88fb4794b4c77
mavlekiev <mavlekiev@gmail.com>
2024-12-24 17:07:02 +0700

    feat: add solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -380,8 +380,42 @@ function getBalanceIndex(arr) {
  *          [10, 9,  8,  7]
  *        ]
  */
-function getSpiralMatrix(/* size */) {
-  throw new Error('Not implemented');
+function getSpiralMatrix(size) {
+  const matrix = [];
+  for (let i = 0; i < size; i += 1) {
+    matrix[i] = [];
+    for (let j = 0; j < size; j += 1) {
+      matrix[i][j] = 0;
+    }
+  }
+  let counter = 1;
+  let startRow = 0;
+  let endRow = size - 1;
+  let startCol = 0;
+  let endCol = size - 1;
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let i = startCol; i <= endCol; i += 1) {
+      matrix[startRow][i] = counter;
+      counter += 1;
+    }
+    startRow += 1;
+    for (let i = startRow; i <= endRow; i += 1) {
+      matrix[i][endCol] = counter;
+      counter += 1;
+    }
+    endCol -= 1;
+    for (let i = endCol; i >= startCol; i -= 1) {
+      matrix[endRow][i] = counter;
+      counter += 1;
+    }
+    endRow -= 1;
+    for (let i = endRow; i >= startRow; i -= 1) {
+      matrix[i][startCol] = counter;
+      counter += 1;
+    }
+    startCol += 1;
+  }
+  return matrix;
 }
 
 /**
@@ -399,8 +433,23 @@ function getSpiralMatrix(/* size */) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const size = matrix.length;
+  const rotatedMatrix = [];
+  const currentMatrix = matrix;
+  for (let i = 0; i < size; i += 1) {
+    rotatedMatrix[i] = matrix[i];
+    for (let j = 0; j < size; j += 1) {
+      rotatedMatrix[i][j] = matrix[i][j];
+    }
+  }
+  for (let i = 0; i < size; i += 1) {
+    currentMatrix[i] = [];
+    for (let j = 0; j < size; j += 1) {
+      currentMatrix[i][j] = rotatedMatrix[size - j - 1][i];
+    }
+  }
+  return matrix;
 }
 
 /**
